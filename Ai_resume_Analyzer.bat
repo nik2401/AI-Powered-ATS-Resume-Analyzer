8c22a5d0b01049f17a6cdf8db8bb940e880cef4e
Nikhil Badoni <nikhilbadoni123@gmail.com>
2025-08-14 23:17:51 +0200

    changes

--- Ai_resume_Analyzer.bat
+++ Ai_resume_Analyzer.bat
@@ -1,36 +1,20 @@
 @echo off
 REM ============================================
-REM HMM POS Tagger for Hindi - Execution Script
+REM AI Powered ATS Analyzer - Execution Script
 REM ============================================
-REM echo Installing dependencies...
-REM pip install -r requirements.txt
-
-REM REM Change to script directory
-REM cd /d "%~dp0"
-
-
-REM if not exist "UD_Hindi-HDTB" (
-    REM echo Downloading Hindi corpus...
-    REM git clone --depth 1 --branch master --single-branch https://github.com/UniversalDependencies/UD_Hindi-HDTB.git UD_Hindi-HDTB
-    
-REM )
-
 
 
 
 echo [IMPORTANT] Remember to configure paths in Setup.py before running!
 echo Open Setup.py and update these variables:
-echo   1. HindiCorpus: Path to training corpus
-echo   2. HindiCorpustest: Path to test corpus
-echo   3. Output.Directory: Where to save results
 
 echo
 
-echo Starting Corpus...
+echo Starting Analyzer...
 
-cd /d "C:\Project\ATS_resume-optimizer-ml"
+cd /d "Project/ATS_resume-optimizer-ml"
 
-set PYTHON_PATH="C:/Users//nikhi/miniconda3/envs/ats/python.exe"
+set PYTHON_PATH="python exe path"  REM Adjust if Python is not in PATH
 
 %PYTHON_PATH% main.py
 
